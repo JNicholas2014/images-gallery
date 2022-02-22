4f514055f6fb4b2edb21e2ccd544013475bec52e
Jeff Nicholas <jeffery.m.nicholas@gmail.com>
2022-02-22 09:24:17 -0600

    styled search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';  
 import Header from './components/Header';
+import Search from './components/Search';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <div>
       <Header title="Images Gallery"/>
+      <Search />
     </div>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/components/Search.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+
+const Search = () => {
+    return (
+        <Container className="mt-4">
+            <Row className="justify-content-center">
+                <Col xs={12} md={8} lg={6}>
+                    <Form>
+                        <Form.Group>
+                            <Row>
+                                <Col xs={9}>
+                                    <Form.Control placeholder="Search for new image..." />
+                                </Col>
+                                <Col>
+                                    <Button variant="primary" type="submit">Search</Button>
+                                </Col>
+                            </Row>
+                        </Form.Group>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default Search;
