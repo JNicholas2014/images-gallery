2662c8041fd419c5a43fe14d89cddbe858f5c514
Jeff Nicholas <jeffery.m.nicholas@gmail.com>
2022-02-22 10:01:03 -0600

    added state to search input

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';  
+import { useState } from 'react';
 import Header from './components/Header';
 import Search from './components/Search';
 
+
+
 const App = () => {
+  const [word, setWord] = useState('');
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  }
   return (
     <div>
       <Header title="Images Gallery"/>
-      <Search />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
     </div>
   );
 };
