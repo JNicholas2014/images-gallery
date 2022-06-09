99227ce89ed2ca04252771a756489d4fbf5a8cbb
Jeff Nicholas <jeffery.m.nicholas@gmail.com>
2022-06-09 10:48:02 -0500

    initial api and python venv creation

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { Navbar, Container } from 'react-bootstrap';
+import React from "react";
+import { Navbar, Container } from "react-bootstrap";
 import { ReactComponent as Logo } from "../images/logo.svg";
 
 const navbarStyle = {
-    backgroundColor: 'lightblue'
+  backgroundColor: "lightblue",
 };
 const Header = ({ title }) => {
-    return (
-      <Navbar style={navbarStyle} variant="light">
-        <Container>
-          <Logo alt={title} style={ { maxWidth: "14rem", maxHeight: "2rem" } }/>
-        </Container>
-      </Navbar>
-    );
+  return (
+    <Navbar style={navbarStyle} variant="light">
+      <Container>
+        <Logo alt={title} style={{ maxWidth: "14rem", maxHeight: "2rem" }} />
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
